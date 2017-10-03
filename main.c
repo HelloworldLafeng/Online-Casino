9dfb766f178622b1dae66a458d19fa8a314fda79
Icegrave0391 <849680403@qq.com>
2017-10-03 23:07:56 +0800

    请无视上面几个脑残版本

--- main.c
+++ main.c
@@ -10,10 +10,13 @@ int main() {
     do{
         scanf("%s",roll);
         flag=1;
-        if(strlen(roll)!=1);                    //输入的并非单字符，你是来捣乱的，滚蛋吧
+        if(strlen(roll)!=1){
+            printf("请您吃点药，输入正确的数据\n");
+            flag=0;
+        }                    //输入的并非单字符，请你好好呆在赌场把钱输光，重新输入
         else if(strlen(roll)==1){
             if(roll[0]=='y')printf("OK\n");
-            if(roll[0]=='q')printf("Bye\n");
+            else if(roll[0]=='q')printf("Bye\n");
             else if(roll[0]=='n')printf("Not Bad\n");
             else {                                       //你是不是不懂规则，为什么不输入y||n，再给你一次机会
                 printf("请您吃点药，输入正确的数据\n");
